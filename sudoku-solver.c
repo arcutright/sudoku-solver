a75ab84803ff8a696578f2f3f0a4a2b3ea42f93b
Aaron Cutright <ahc49@case.edu>
2016-11-28 18:15:49 -0500

    Finished le code it is bad has never solved a puzzle super inefficient i'll leave fixing it up to a final version

--- sudoku-solver.c
+++ sudoku-solver.c
@@ -9,7 +9,7 @@
 
 #define LENGTH(x)   (sizeof(x) / sizeof((x)[0])) //length of array
 #define LIMIT       9 //max rows, max columns
-#define MAXRETRIES  3 //max retries for each thread before exiting
+#define MAXRETRIES  500 //max retries for each thread before exiting
 #define INPUTFILE   "file/in.txt"
 #define OUTPUTFILE  "file/out.txt"
 
@@ -57,16 +57,31 @@ int main (void) {
 	memcpy(reference, board, sizeof(int)*LIMIT*LIMIT);
 
 	//set up thread parameters
-	pthread_t tid, tid2, tid3, tid4, tid5;
-	pthread_attr_t attr, attr2, attr3, attr4, attr5;
+	pthread_t tid, tid2, tid3, tid4, tid5, tid6, tid7, tid8, tid9,
+		tid10, tid11, tid12, tid13, tid14, tid15, tid16, tid17;
+	pthread_attr_t attr, attr2, attr3, attr4, attr5, attr6, attr7,
+		attr8, attr9, attr10, attr11, attr12, attr13, attr14, 
+		attr15, attr16, attr17;
 	pthread_attr_init(&attr);
 	pthread_attr_init(&attr2);
 	pthread_attr_init(&attr3);
 	pthread_attr_init(&attr4);
 	pthread_attr_init(&attr5);
+	pthread_attr_init(&attr6);
+	pthread_attr_init(&attr7);
+	pthread_attr_init(&attr8);
+	pthread_attr_init(&attr9);
+	pthread_attr_init(&attr10);
+	pthread_attr_init(&attr11);
+	pthread_attr_init(&attr12);
+	pthread_attr_init(&attr13);
+	pthread_attr_init(&attr14);
+	pthread_attr_init(&attr15);
+	pthread_attr_init(&attr16);
+	pthread_attr_init(&attr17);
 
-	boardCursor c1 = {8, 0};
-	pthread_create(&tid, &attr, solveSection, &c1);
+	boardCursor c = {8, 0};
+	pthread_create(&tid, &attr, solveSection, &c);
 	boardCursor c2 = {7, 0};
 	pthread_create(&tid2, &attr2, solveSection, &c2);
 	boardCursor c3 = {6, 0};
@@ -75,6 +90,30 @@ int main (void) {
 	pthread_create(&tid4, &attr4, solveSection, &c4);
 	boardCursor c5 = {4, 0};
 	pthread_create(&tid5, &attr5, solveSection, &c5);
+	boardCursor c6 = {3, 0};
+	pthread_create(&tid6, &attr6, solveSection, &c6);
+	boardCursor c7 = {2, 0};
+	pthread_create(&tid7, &attr7, solveSection, &c7);
+	boardCursor c8 = {1, 0};
+	pthread_create(&tid8, &attr8, solveSection, &c8);
+	boardCursor c9 = {0, 0};
+	pthread_create(&tid9, &attr9, solveSection, &c9);
+	boardCursor c10 = {0, 1};
+	pthread_create(&tid10, &attr10, solveSection, &c10);
+	boardCursor c11 = {0, 2};
+	pthread_create(&tid11, &attr11, solveSection, &c11);
+	boardCursor c12 = {0, 3};
+	pthread_create(&tid12, &attr12, solveSection, &c12);
+	boardCursor c13 = {0, 4};
+	pthread_create(&tid13, &attr13, solveSection, &c13);
+	boardCursor c14 = {0, 5};
+	pthread_create(&tid14, &attr14, solveSection, &c14);
+	boardCursor c15 = {0, 6};
+	pthread_create(&tid15, &attr15, solveSection, &c15);
+	boardCursor c16 = {0, 7};
+	pthread_create(&tid16, &attr16, solveSection, &c16);
+	boardCursor c17 = {0, 8};
+	pthread_create(&tid17, &attr17, solveSection, &c17);
 
 	//join all threads
 	pthread_join(tid, NULL);
@@ -82,6 +121,18 @@ int main (void) {
 	pthread_join(tid3, NULL);
 	pthread_join(tid4, NULL);
 	pthread_join(tid5, NULL);
+	pthread_join(tid6, NULL);
+	pthread_join(tid7, NULL);
+	pthread_join(tid8, NULL);
+	pthread_join(tid9, NULL);
+	pthread_join(tid10, NULL);
+	pthread_join(tid11, NULL);
+	pthread_join(tid12, NULL);
+	pthread_join(tid13, NULL);
+	pthread_join(tid14, NULL);
+	pthread_join(tid15, NULL);
+	pthread_join(tid16, NULL);
+	pthread_join(tid17, NULL);
 
 	//printBoard(board);
 	printBoardComparison(reference, board);
@@ -103,7 +154,7 @@ void *solveSection (void *cursorContext) {
 	else
 		toSolve = LIMIT - col;
 	//keep going until all needed cells are solved or num of retries too high
-	int retries = 0;
+	long long retries = 0;
 	int prevSolved = 0;
 	while (solved < toSolve && retries < MAXRETRIES) {
 		//progress diagonally
@@ -111,7 +162,7 @@ void *solveSection (void *cursorContext) {
 			sem_wait(&mutex);
 			//solve cell
 			if (board[row][col] == -1) {
-				bool used[LIMIT]; //tracks which numbers can't be candidates
+				bool used[10]; //tracks which numbers can't be candidates
 				int r, c;
 				//eliminate options in rows and columns
 				for (r=0; r < LIMIT; r++) {
@@ -135,7 +186,7 @@ void *solveSection (void *cursorContext) {
 				}
 				//check if solution was found
 				int i, temp, sum = 0;
-				for (i=0; i < LIMIT; i++){
+				for (i=1; i < 10; i++){
 					sum += !used[i];
 					if(!used[i])
 						temp = i;
@@ -155,8 +206,11 @@ void *solveSection (void *cursorContext) {
 			retries++;
 		else
 			retries = 0;
+		//if (retries % (MAXRETRIES/100) == 0)
+		//	printf("%.0Lf%% ", (long double)retries*100.0/MAXRETRIES);
 		prevSolved = solved;
 	}
+	//printf("\n");
 	pthread_exit(0);
 }
 
